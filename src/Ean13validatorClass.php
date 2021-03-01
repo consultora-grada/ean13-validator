d0d274beb80b5558c52d25cedccd24290775b852
Carlos Rubén Jacobs <carlosjac@gmail.com>
2021-03-01 18:16:33 -0300

    implement validator

--- src/Ean13validatorClass.php
+++ src/Ean13validatorClass.php
@@ -4,4 +4,31 @@ namespace Grada\Ean13validator;
 
 class Ean13validatorClass
 {
+    function validate_EAN13Barcode(string $barcode)
+    {
+        if (!preg_match("/^[0-9]{13}$/", $barcode)) {
+            return false;
+        }
+    
+        $digits = $barcode;
+    
+        $even_sum = $digits[1] + $digits[3] + $digits[5] +
+                    $digits[7] + $digits[9] + $digits[11];
+    
+        $even_sum_three = $even_sum * 3;
+    
+        $odd_sum = $digits[0] + $digits[2] + $digits[4] +
+                   $digits[6] + $digits[8] + $digits[10];
+    
+        $total_sum = $even_sum_three + $odd_sum;
+    
+        $next_ten = (ceil($total_sum / 10)) * 10;
+        $check_digit = $next_ten - $total_sum;
+    
+        if ($check_digit == $digits[12]) {
+            return true;
+        }
+    
+        return false;
+    }
 }
---
+++ tests/Ean13ValidatorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Grada\Ean13validator\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Grada\Ean13validator\Ean13validatorClass;
+
+class Ean13ValidatorTest extends TestCase
+{
+    /** @test */
+    public function it_can_check_invalid_length()
+    {
+        $vc = new Ean13validatorClass();
+        $result = $vc->validate_EAN13Barcode("0123456789");
+        $this->assertFalse($result);
+    }
+
+    /** @test */
+    public function it_can_check_invalid_digits()
+    {
+        $vc = new Ean13validatorClass();
+        $result = $vc->validate_EAN13Barcode("0a23456789123");
+        $this->assertFalse($result);
+    }
+
+    /** @test */
+    public function it_can_check_invalid_check_digit()
+    {
+        $vc = new Ean13validatorClass();
+        $result = $vc->validate_EAN13Barcode("7790040111003");
+        $this->assertFalse($result);
+    }
+
+    /** @test */
+    public function it_can_check_invalid_code()
+    {
+        $vc = new Ean13validatorClass();
+        $result = $vc->validate_EAN13Barcode("7790040111014");
+        $this->assertFalse($result);
+    }
+        
+    /** @test */
+    public function it_can_check_valid()
+    {
+        $vc = new Ean13validatorClass();
+        $result = $vc->validate_EAN13Barcode("7790040111004");
+        $this->assertTrue($result);
+    }
+    
+
+}
