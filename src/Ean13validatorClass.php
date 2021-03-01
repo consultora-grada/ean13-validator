2b6c1662b3bf49309f2614c74645f3f5b70ef3bf
Carlos Rubén Jacobs <carlosjac@gmail.com>
2021-03-01 18:31:43 -0300

    fix

--- src/Ean13validatorClass.php
+++ src/Ean13validatorClass.php
@@ -10,22 +10,22 @@ class Ean13validatorClass
             return false;
         }
     
-        $digits = intval($barcode);
+        $digits = $barcode;
     
-        $even_sum = $digits[1] + $digits[3] + $digits[5] +
-                    $digits[7] + $digits[9] + $digits[11];
+        $even_sum = intval($digits[1]) + intval($digits[3]) + intval($digits[5]) +
+        intval($digits[7]) + intval($digits[9]) + intval($digits[11]);
     
         $even_sum_three = $even_sum * 3;
     
-        $odd_sum = $digits[0] + $digits[2] + $digits[4] +
-                   $digits[6] + $digits[8] + $digits[10];
+        $odd_sum = intval($digits[0]) + intval($digits[2]) + intval($digits[4]) +
+        intval($digits[6]) + intval($digits[8]) + intval($digits[10]);
     
         $total_sum = $even_sum_three + $odd_sum;
     
         $next_ten = (ceil($total_sum / 10)) * 10;
         $check_digit = $next_ten - $total_sum;
     
-        if ($check_digit == $digits[12]) {
+        if ($check_digit == intval($digits[12])) {
             return true;
         }
     
